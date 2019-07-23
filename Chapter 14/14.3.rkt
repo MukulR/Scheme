cc43379b91d5ab5448d8567c561107a3c6e476c7
mukulibukuli <mukulgrao@gmail.com>
2019-07-22 18:54:38 -0700

    Update 14.3.rkt

--- Chapter 14/14.3.rkt
+++ Chapter 14/14.3.rkt
@@ -5,3 +5,8 @@
       (if (member? (first phrase) (butfirst phrase))
           (sentence (remove-duplicates (butfirst phrase)))
           (sentence (first phrase) (remove-duplicates (butfirst phrase))))))
+
+(define (member? wd phrase)
+  (cond ((= (count phrase) 0) #f)
+        ((equal? wd (first phrase)) #t)
+        (else (member? wd (butfirst phrase)))))
